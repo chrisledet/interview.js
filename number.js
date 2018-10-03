f78f6be10a71b6bea4d4101c642979552506702b
Chris Ledet <me@chrisledet.com>
2018-10-03 11:58:26 -0700

    Update number.js

--- number.js
+++ number.js
@@ -10,6 +10,12 @@ class Node {
 	The digits are stored in reverse order and each of their nodes contain a single digit.
 
 	Add the two numbers and return it as a linked list.
+
+	Example:
+	- leftNode:  Node(1) -> Node(2) -> Node(3)    // represents 321
+	- rightnode: Node(2) -> Node(8) -> Node(1)    // represents 182
+
+	- return:    Node(3) -> Node(0) -> Node(5)    // represents 503
 */
 Add = function(leftNode, rightNode) {
 	// TODO: implemenet me!!!!
